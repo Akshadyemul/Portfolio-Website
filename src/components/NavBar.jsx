a0d3f8dcf99815d1367c617b15f2f6613754b0be
Akshadyemul <akshadyemul05@gmail.com>
2025-11-29 17:16:25 +0530

    Creating Portfolio Website

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <ul className='flex gap-6 justify-center p-3'>
+        <li>Home</li>
+        <li>Project</li>
+        <li>Experience</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
