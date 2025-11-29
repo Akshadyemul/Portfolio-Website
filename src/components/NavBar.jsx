083504da64f93d7ca104a0290c39a12e1398364e
Akshadyemul <akshadyemul05@gmail.com>
2025-11-29 18:18:09 +0530

    NavBar changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function NavBar() {
   return (
     <div>
+      <h1>Akshad Yemul</h1>
       <ul className='flex gap-6 justify-center p-3'>
         <li>Home</li>
         <li>Project</li>
@@ -10,6 +11,7 @@ function NavBar() {
         <li>Skills</li>
         <li>Contact</li>
       </ul>
+      <h1>Login</h1>
     </div>
   )
 }
